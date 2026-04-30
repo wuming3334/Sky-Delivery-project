e35972942453915fd77bf464ef04af89ef3617c5
wuming3334 <2868037311@qq.com>
2026-04-30 11:30:12 +0800

    地址簿开发

--- sky-server/src/main/java/com/sky/controller/User/DishController.java
+++ sky-server/src/main/java/com/sky/controller/User/DishController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,7 +35,7 @@ public class DishController {
      */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
-    public Result<List<DishVO>> list(Long categoryId) {
+    public Result<List<DishVO>> list(@RequestParam Long categoryId) {
         log.info("根据分类id查询菜品：{}", categoryId);
         //使用redis来缓存菜品信息,先判断是否有redis缓存
         String key = "dish_" + categoryId;
